82b82dc3b95ad750d872c76abd975adf8d66b35c
Philippe Gras <philippe.gras@cern.ch>
2024-07-29 15:29:55 +0200

    Add --update option to generate.jl

--- generate.jl
+++ generate.jl
@@ -12,8 +12,34 @@ makefile = "Makefile.in"
 buildfile = "build.jl.in"
 jlsrc = ["iROOT.jl", "ROOT.jl", "ROOTex.jl", "demo.jl", "def_args.jl" ]
 
+updatemode = ("--update" ∈ ARGS)
+updatemode && println("Update mode")
+noclean = ("--noclean" ∈ ARGS)
+
+function samecontents(fpath1, fpath2)
+    f1 = open(fpath1)
+    f2 = try
+        open(fpath2)
+    catch
+        return false
+    end
+    same = true
+    while(same && !eof(f1) && !eof(f2))
+        same &= (read(f1, Char) != read(f2, Char))
+    end
+    same &= eof(f1) ⊻ eof(f2) #different sizes
+    same
+end
+
+function cp_if_differs(fpath1, fpath2; kwargs...)
+    samecontents(fpath1, fpath2) && return false
+    cp(fpath1, fpath2; kwargs...)
+end
+
+const cpfunc = updatemode ? cp_if_differs : cp
+
 # It is important to start from an empty src directory. Require to move all previously generated file
-if isdir(joinpath(builddir, "ROOT"))
+if !updatemode && isdir(joinpath(builddir, "ROOT"))
     @error("Directory " * joinpath(builddir, "ROOT") * " is on the way. Please remove it and start again.")
     exit(1)
 end
@@ -26,11 +52,14 @@ root_version = readchomp(`root-config --version`)
 
 open(wit, "w") do f
     for l in eachline(witin)
-	println(f, replace(l, "%ROOT_INC_DIR%" => rootincdir))
+	println(f, replace(l, "%ROOT_INC_DIR%" => rootincdir,
+                           "%CLEAN%" => (noclean ? "false" : "true")))
     end
 end
 
-rc = wrapit(wit, force=true, cmake=true, output_prefix=builddir)
+rc = wrapit(wit, force=true, cmake=true, output_prefix=builddir, 
+            update=updatemode, verbosity=10)
+
 if !isnothing(rc) && rc != 0
     println(stderr, "Failed to produce wrapper code with the wrapit function. Exited with code ", rc, ".")
     exit(rc)
@@ -40,25 +69,40 @@ run(`$(@__DIR__)/postfix.sh $builddir/ROOT/deps/src`)
 
 function edit_file(file, map)
     tmpfile = joinpath(dirname(file),  "#" * basename(file) * "#")
+    changed = false
     try
         open(tmpfile, "w") do fout
             open(file, "r") do fin
                 for l in eachline(fin)
-                    println(fout, replace(l, map))
+                    newl = replace(l, map)
+                    println(fout, newl)
+                    changed |= (newl != l)
                 end
             end
         end
-        mv(tmpfile, file, force=true)
+        #Preserve file timestamp if there was no change:
+        if(changed)
+            mv(tmpfile, file, force=true)
+        else
+            rm(tmpfile)
+        end
     catch e
         @error "Failed to edit file $file. $(e.msg)"
     end
+    changed
 end
 
 #Code patch: _IO_FILE => FILE (_IO_FILE is not defined on macOS and FILE is anyway cleaner)
-let d = joinpath(builddir, "ROOT", "deps", "src")
+let d = joinpath(builddir, "ROOT", "deps", "src"), modif = false
     for f in readdir(d)
         f = joinpath(d, f)
-        isfile(f) && edit_file(f, "_IO_FILE" => "FILE")
+        isfile(f) && (modif = edit_file(f, "_IO_FILE" => "FILE"))
+    end
+    #in 1 file per class mode, we need to also change the file name:
+    oldname = joinpath(d, "Jl_IO_FILE.cxx")
+    newname = joinpath(d, "JlFILE.cxx")
+    if isfile(oldname) && (!updatemode || !isfile(newname) || !samecontents(oldname, newname))
+        mv(oldname, newname, force=true)
     end
 end
 
@@ -75,25 +119,27 @@ end
 
 err && exit(1)
 
-#open(joinpath(builddir, "ROOT", "deps", "wrapit.cmake"), "w") do fn
-#    todrop = builddir * "/ROOT/deps/"
-#    for l in eachline(joinpath(builddir, "wrapit.cmake"))
-#	println(f, replace(l, todrop => ""))
-#    end
-#    rm(joinpath(builddir, "wrapit.cmake"))
-#end
+if !updatemode
+    open(joinpath(builddir, "ROOT", "deps", "wrapit.cmake"), "w") do f
+        todrop = builddir * "/ROOT/deps/"
+        for l in eachline(joinpath(builddir, "wrapit.cmake"))
+	    println(f, replace(l, todrop => ""))
+        end
+        rm(joinpath(builddir, "wrapit.cmake"))
+    end
+end
 
-rm(joinpath(builddir, "ROOT", "src", "ROOT-generated.jl"), force=true)
+rm(joinpath(builddir, "ROOT", "src", "ROOT-generated.jl"))
 
 for f in cxxsrc
-    cp(joinpath("src", f), joinpath(builddir, "ROOT", "deps", "src", f), force=true)
+    cpfunc(joinpath("src", f), joinpath(builddir, "ROOT", "deps", "src", f), force=true)
 end
 
 #cp(joinpath("src", buildfile), joinpath(builddir, "ROOT", "deps", replace(buildfile, r"\.in$" => "")), force=true)
 open(joinpath("src", buildfile)) do fin
     open(joinpath(builddir, "ROOT", "deps", replace(buildfile, r"\.in$" => "")), "w") do fout
         for l in eachline(fin)
-            println(fout, replace(l, "%ROOT_VERSION%" => root_version))
+            println(fout, replace(l, "%ROOT_VERSION%" => root_version, "%CLEAN%"=> !noclean))
         end
     end
 end
