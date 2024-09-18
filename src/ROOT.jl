7e3212de9b657709e56a18f9396c2ae923c4d21e
Philippe Gras <philippe.gras@cern.ch>
2024-09-18 14:47:52 +0200

    Fix "missing assert.h" error

--- src/ROOT.jl
+++ src/ROOT.jl
@@ -26,11 +26,33 @@ include("iROOT.jl")
 
 TF1!kDefault = 0
 
+module Internals
+import Conda
+function get_conda_build_sysroot()
+    cxx = joinpath(Conda.PREFIX, "bin", "c++")
+    cmd = `$cxx -DNDEBUG -xc++ -E -v /dev/null`
+    sysroot=""
+    err = Pipe()
+    run(pipeline(ignorestatus(cmd), stdout=devnull, stderr=err), wait=true)
+    close(err.in)
+    for l in eachline(err)
+        if occursin(r"sysroot/usr/include$", l)
+            sysroot = strip(l)
+            break
+        end
+    end
+    replace(normpath(sysroot), normpath("/usr/include") => "")
+end
+end
+
+import .Internals
+
 function __init__()
     # Some required environment cleanup before loading the ROOT libraries
     saved_path = ENV["PATH"]
     saved_ld_library_path = get(ENV, "LD_LIBRARY_PATH", nothing)
     saved_dyld_library_path = get(ENV, "DYLD_LIBRARY_PATH", nothing)
+    saved_conda_build_rootsys = get(ENV, "CONDA_BUILD_SYSROOT", nothing)
     #   Prevent mix-up of root library version is another version than ours is in LD_LIBRARY_PATH:
     isnothing(saved_ld_library_path) || (ENV["LD_LIBRARY_PATH"] = "")
     isnothing(saved_dyld_library_path) || (ENV["DYLD_LIBRARY_PATH"] = "")
@@ -39,14 +61,16 @@ function __init__()
     #   missing in the PATH list. In the Conda install, compiler is same directory as ROOT
     #   binaries, rootbindir
     ENV["PATH"] *= ":" * rootbindir
-
+    #Fix missing not-found header issue, "assert.h not found": 
+    ENV["CONDA_BUILD_SYSROOT"] = Internals.get_conda_build_sysroot()
     @initcxx
     global gROOT = ROOT!GetROOT()
 
     #Restore the environment:
     ENV["PATH"] = saved_path
-    isnothing(saved_ld_library_path) || (ENV["LD_LIBRARY_PATH"] = saved_ld_library_path)
-    isnothing(saved_dyld_library_path) || (ENV["DYLD_LIBRARY_PATH"] = saved_dyld_library_path)
+    if isnothing(saved_ld_library_path); delete!(ENV, "LD_LIBRARY_PATH") else (ENV["LD_LIBRARY_PATH"] = saved_ld_library_path); end
+    if isnothing(saved_dyld_library_path);  delete!(ENV, "DYLD_LIBRARY_PATH"); else (ENV["DYLD_LIBRARY_PATH"] = saved_dyld_library_path); end
+    if isnothing(saved_conda_build_rootsys);  delete!(ENV, "CONDA_BUILD_SYSROOT"); else (ENV["CONDA_BUILD_SYSROOT"] = saved_conda_build_rootsys); end
 
     isinteractive() && _init_event_loop()
 end
