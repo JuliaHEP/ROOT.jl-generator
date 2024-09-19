9a9320b542674d78e50f67a312d641a6e3c7d4ed
Philippe Gras <philippe.gras@cern.ch>
2024-09-19 08:59:34 +0200

    Code simplification

--- src/ROOT.jl
+++ src/ROOT.jl
@@ -49,29 +49,20 @@ import .Internals
 
 function __init__()
     # Some required environment cleanup before loading the ROOT libraries
-    saved_path = ENV["PATH"]
-    saved_ld_library_path = get(ENV, "LD_LIBRARY_PATH", nothing)
-    saved_dyld_library_path = get(ENV, "DYLD_LIBRARY_PATH", nothing)
-    saved_conda_build_rootsys = get(ENV, "CONDA_BUILD_SYSROOT", nothing)
-    #   Prevent mix-up of root library version is another version than ours is in LD_LIBRARY_PATH:
-    isnothing(saved_ld_library_path) || (ENV["LD_LIBRARY_PATH"] = "")
-    isnothing(saved_dyld_library_path) || (ENV["DYLD_LIBRARY_PATH"] = "")
-    #   Workaroud to prevent a crash with root installed with Conda linker to
-    #   the c++ compiler called by cling to get the include directories and
-    #   missing in the PATH list. In the Conda install, compiler is same directory as ROOT
-    #   binaries, rootbindir
-    ENV["PATH"] *= ":" * rootbindir
-    #Fix missing not-found header issue, "assert.h not found": 
-    ENV["CONDA_BUILD_SYSROOT"] = Internals.get_conda_build_sysroot()
-    @initcxx
-    global gROOT = ROOT!GetROOT()
-
-    #Restore the environment:
-    ENV["PATH"] = saved_path
-    if isnothing(saved_ld_library_path); delete!(ENV, "LD_LIBRARY_PATH") else (ENV["LD_LIBRARY_PATH"] = saved_ld_library_path); end
-    if isnothing(saved_dyld_library_path);  delete!(ENV, "DYLD_LIBRARY_PATH"); else (ENV["DYLD_LIBRARY_PATH"] = saved_dyld_library_path); end
-    if isnothing(saved_conda_build_rootsys);  delete!(ENV, "CONDA_BUILD_SYSROOT"); else (ENV["CONDA_BUILD_SYSROOT"] = saved_conda_build_rootsys); end
-
+    withenv(
+        #   Prevent mix-up of root library version is another version than ours is in LD_LIBRARY_PATH:
+        "LD_LIBRARY_PATH" => "",
+        "DYLD_LIBRARY_PATH" => "",
+        #   Workaroud to prevent a crash with root installed with Conda linker to
+        #   the c++ compiler called by cling to get the include directories and
+        #   missing in the PATH list. In the Conda install, compiler is same directory as ROOT
+        #   binaries, rootbindir
+        "PATH" => ENV["PATH"] * ":" * rootbindir,
+        #   Fix "assert.h not found" issue:
+        "CONDA_BUILD_SYSROOT" => Internals.get_conda_build_sysroot()) do
+            @initcxx
+            global gROOT = ROOT!GetROOT()
+        end
     isinteractive() && _init_event_loop()
 end
 
